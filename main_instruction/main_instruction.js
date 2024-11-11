ea9456d1b5d0ddf694614c8ae7c20dc61e3c3619
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-11-11 09:59:12 +0100

    #5 feat Premier composant fonctionnel

--- main_instruction/main_instruction.js
+++ main_instruction/main_instruction.js
@@ -2,57 +2,29 @@ class MainInstruction extends HTMLElement {
 
     constructor() {
         super();
+
+        let shadowRoot = this.attachShadow({mode: "open"});
+
+        fetch("/main_instruction/main_instruction.html")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Échec du chargement du fichier HTML');
+                }
+                return response.text();
+            })
+            .then(htmlContent => {
+                let templateContent = new DOMParser().parseFromString(htmlContent, "text/html").querySelector("template").content;
+                shadowRoot.appendChild(templateContent.cloneNode(true));
+                this.setupLogic(); // Appeler la fonction pour configurer la logique du composant
+            })
+            .catch(error => {
+                console.error("Erreur lors du chargement du fichier HTML : ", error);
+            });
     }
 
-    connectedCallback() {
-        // Create a shadow root
-        const shadow = this.attachShadow({mode: "open"});
-
-        // Create spans
-        const wrapper = document.createElement("div");
-        wrapper.setAttribute("class", "wrapper");
-
-        const icon = document.createElement("div");
-        icon.setAttribute("class", "icon");
-        icon.setAttribute("tabindex", 0);
-        
-        const info = document.createElement("div");
-        info.setAttribute("class", "info");
-
-        const info1 = document.createElement("div");
-        info1.setAttribute("class", "info");
-
-        // Insert icon
-        let imgUrl = this.getAttribute("img");
-
-        const img = document.createElement("img");
-        img.src = imgUrl;
-        icon.appendChild(img);
-
-        // Take attribute content and put it inside the info span
-        const distance = this.getAttribute("data-distance");
-        info.textContent = distance + "m";
-
-        const text = this.getAttribute("data-text");
-        info1.textContent = text;
-
-        // Create some CSS
-        const style = document.createElement("style");
-        console.log(style.isConnected);
-
-        style.textContent = `
-            .info {
-                background: red;
-            }
-        `;
-
-        // Attach the created elements to the shadow dom
-        shadow.appendChild(style);
-        console.log(style.isConnected);
-        shadow.appendChild(wrapper);
-        wrapper.appendChild(icon);
-        wrapper.appendChild(info);
-        wrapper.appendChild(info1);
+    setupLogic() {
+        /* Vrai constructeur avec EventListener, ...*/
+        alert("Le composant est maintenant visible sur la page !");
     }
 }
 
