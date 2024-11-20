facb0663e17e960e0aeefc441b71e82465fa5bf9
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-11-20 16:08:34 +0100

    #9 feat Composant existant

--- goal/goal.js
+++ goal/goal.js
@@ -1,7 +1,30 @@
 class Goal extends HTMLElement {
+
     constructor() {
         super();
+    
+        let shadowRoot = this.attachShadow({mode: "open"});
+
+        fetch("/goal/goal.html")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Échec du chargement du fichier HTML');
+                }
+                return response.text();
+            })
+            .then(htmlContent => {
+                let templateContent = new DOMParser().parseFromString(htmlContent, "text/html").querySelector("template").content;
+                shadowRoot.appendChild(templateContent.cloneNode(true));
+                this.setupLogic(); // Appeler la fonction pour configurer la logique du composant
+            })
+            .catch(error => {
+                console.error("Erreur lors du chargement du fichier HTML : ", error);
+            });
+    }
+
+    setupLogic() {
+        /* Vrai constructeur avec EventListener, ...*/
     }
 }
 
-customElements.define("goal", Goal);
+customElements.define("goal-component", Goal);
