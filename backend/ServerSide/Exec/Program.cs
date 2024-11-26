a459b21f93f997590defbb89548791cf24947503
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-11-26 10:34:18 +0100

    #18 feat Initialisation du server Server Side

---
+++ backend/ServerSide/Exec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Router.RoutingServerClient client = new Router.RoutingServerClient();
+            Console.WriteLine(client.GetPath("a", "b"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ backend/ServerSide/ServerSide/IRoutingServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServerSide
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IRoutingServer
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "path?start={start}&end={end}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string GetPath(string start, string end);
+    }
+
+    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+    // Vous pouvez ajouter des fichiers XSD au projet. Une fois le projet généré, vous pouvez utiliser directement les types de données qui y sont définis, avec l'espace de noms "ServerSide.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
